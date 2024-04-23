bc41788f4c7e597cce87320b7510fc8c3b1704d8
amine-zzr <aminezoukri@gmail.com>
2024-04-23 05:20:49 +0000

    task instructions are very unclear

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 #installing a package using Puppet
+
 package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
