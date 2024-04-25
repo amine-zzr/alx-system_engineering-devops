2c320774a7e2a74656b56b6e26562426d687324a
amine-zzr <aminezoukri@gmail.com>
2024-04-25 00:43:58 +0000

    installin and configuring nginx using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,42 @@
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Define Nginx service
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Configure Nginx server block
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html;
+    
+    location / {
+        return 200 'Hello World!\n';
+    }
+
+    location /redirect_me {
+        return 301 https://github.com/amine-zzr;
+    }
+}
+",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Enable default server block
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
+}
