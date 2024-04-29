34db69a74cde210a181c278a782f6fa67d3404e1
amine-zzr <aminezoukri@gmail.com>
2024-04-29 17:57:37 +0000

    a manifest to install and configure Nginx

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,41 @@
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Define Nginx service
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Configure Nginx server block
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html;
+    
+    location / {
+        return 200 'Hello World!\n';
+    }
+
+    add_header X-Served-By $hostname;
+
+}
+",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Enable default server block
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
+}
