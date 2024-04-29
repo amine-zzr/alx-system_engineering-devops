d8f2a189aef820e01d4bee914f079d591c984e15
amine-zzr <aminezoukri@gmail.com>
2024-04-29 18:05:10 +0000

    fixing code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,36 +3,39 @@ package { 'nginx':
   ensure => installed,
 }
 
-# Configure Nginx server
+# Define Nginx service
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Configure Nginx server block
 file { '/etc/nginx/sites-available/default':
+  ensure  => file,
   content => "
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html;
     
-        root /var/www/html;
-        index index.html;
-    
-        location / {
-            return 200 'Hello World!\n';
-        }
-    
-        add_header X-Served-By $hostname;
+    location / {
+        return 200 'Hello World!\n';
     }
-  ",
+
+    add_header X-Served-By ${hostname};
+
+}
+",
+  require => Package['nginx'],
   notify  => Service['nginx'],
 }
 
-# Enable the default site
+# Enable default server block
 file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
-}
-
-# Restart Nginx service after configuration changes
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
 }
