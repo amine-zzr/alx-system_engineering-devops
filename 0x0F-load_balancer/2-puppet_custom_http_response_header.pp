59fbcceefdc9f636dd88f148ca21b78dd0980a30
amine-zzr <aminezoukri@gmail.com>
2024-05-05 15:27:25 +0000

    ?

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,16 @@
-exec { 'apt-update':
-  command => '/usr/bin/apt update',
-  refreshonly => true,  # Only execute when needed
-}
+# Puppet manifests is a headache
 
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-update'],  # Ensure apt-update runs first
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],  # Ensure nginx is installed first
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Add a custom header to nginx configuration
-file_line { 'nginx_custom_header':
-  path    => '/etc/nginx/sites-available/default',
-  line    => '    add_header X-Served-By $hostname;',
-  match   => '^    location / {$',
-  after   => '^    location / {$',
-  require => Package['nginx'],
-  notify  => Service['nginx'],  # Restart nginx when the config changes
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-service { 'nginx':
-  ensure => running,
-  enable => true,
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
