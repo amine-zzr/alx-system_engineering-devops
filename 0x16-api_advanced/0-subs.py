eb071e4bb5268d442480de735cf6171dc2653f91
amine-zzr <aminezoukri@gmail.com>
2024-06-04 19:00:21 +0000

    fixing style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """
-a function that queries the Reddit API and returns
-the number of subscribers
+A function that queries the Reddit API
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number
-        of subscribers for a given subreddit.
-    If an invalid subreddit is given, returns 0.
+    of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the given subreddit.
+
+    Return:
+        Int: the numberof subscribers for a given subreddit.
+        If an invalid subreddit is given, returns 0.
     """
     headers = {'User-Agent': 'Amine API Client/1.0'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
